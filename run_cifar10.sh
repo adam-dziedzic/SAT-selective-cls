ba1e1410ecd34714ede0b38f693eb5268bbf64d7
Adam Dziedzic <adam.cajf@gmail.com>
2022-08-07 14:06:10 -0400

    run_cifar10

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -1,21 +1,42 @@
-ARCH=vgg16_bn
-LOSS=sat
-DATASET=cifar10
-PRETRAIN=150
-MOM=0.9
-SAVE_DIR='./log/'${DATASET}_${ARCH}_${LOSS}_$1
-GPU_ID=1
+#!/bin/bash
 
-mkdir -p ./log
+#SBATCH --qos=high
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10g
+#SBATCH -c 4
+#SBATCH --partition=t4v2
+#SBATCH --job-name=train-cifar10-vgg16_bn_ce_loss
+#SBATCH --output=train-imagenet.out
+#SBATCH --array=0-25
+# everything below this line is optional, but are nice to have quality of life things
+#SBATCH --output=job.%J.out # tell it to store the output console text to a file called job.<assigned job number>.out
+#SBATCH --error=job.%J.err # tell it to store the error messages from the program (if it doesn't write them to normal console output) to a file called job.<assigned job muber>.err
+#SBATCH --jobname="train cifar10 vgg16_bn ce_loss" # a nice readable name to give your job so you know what it is when you see it in the queue, instead of just numbers
+
+# source /ssd003/home/ady/.bashrc
+# conda activate /ssd003/home/ady/envnew
+source /h/ady/.envnew
+
+ARCH=vgg16_bn
+LOSS=ce
+DATASET=cifar10
+PRETRAIN=0
+MOM=0.9
+SAVE_DIR='/ssd003/home/ady/'${DATASET}_${ARCH}_${LOSS}
+GPU_ID=0
+
+mkdir -p ${SAVE_DIR}
 
 ### train
-python -u train.py --arch ${ARCH} --gpu-id ${GPU_ID} --pretrain ${PRETRAIN} --sat-momentum ${MOM} \
-       --loss ${LOSS} \
-       --dataset ${DATASET} --save ${SAVE_DIR}  \
-       2>&1 | tee -a ${SAVE_DIR}.log
+python -u train.py --arch ${ARCH} --gpu-id ${GPU_ID} \
+--pretrain ${PRETRAIN} --sat-momentum ${MOM} \
+--loss ${LOSS} \
+--dataset ${DATASET} --save ${SAVE_DIR} \
+--manual_seed ${SLURM_ARRAY_TASK_ID} \
+2>&1 | tee -a ${SAVE_DIR}.log
 
 ### eval
 python -u train.py --arch ${ARCH} --gpu-id ${GPU_ID} \
-       --loss ${LOSS} --reward ${REWARD} --dataset ${DATASET} \
-       --save ${SAVE_DIR} --evaluate \
-       2>&1 | tee -a ${SAVE_DIR}.log
+--loss ${LOSS} --reward ${REWARD} --dataset ${DATASET} \
+--save ${SAVE_DIR} --evaluate \
+2>&1 | tee -a ${SAVE_DIR}.log
